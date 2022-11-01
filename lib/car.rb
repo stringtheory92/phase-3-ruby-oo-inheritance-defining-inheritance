148ead67f1e890ada1dede8b26c3707d0b54e1dd
Adam Kabak <adamkabak@yahoo.com>
2022-11-01 07:15:42 -0400

    commit tree 6fb791605f0348349b7f30d39479d61389682121

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,12 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+require 'pry'
+
+class Car < Vehicle
+    def go 
+        "pput put put put"
+    end
+end
+
+car = Car.new(5, 5)
+binding.pry
